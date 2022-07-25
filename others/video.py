891b488b85bc4c08d28ac8de4b5cb475c459144e
jonathanavc <jonvenega@gmail.com>
2022-07-25 02:03:34 -0400

    a

---
+++ others/video.py
@@ -0,0 +1,18 @@
+import cv2
+
+capture = cv2.VideoCapture('Popeye the Sailor Meets Sindbad the Sailor.mp4')
+cont = 0
+path = ""
+
+while (capture.isOpened()):
+    ret, frame = capture.read()
+    if (ret == True):
+        cv2.imwrite(path + 'IMG_%04d.jpg' % cont, frame)    
+        cont += 1
+        if (cv2.waitKey(1) == ord('s')):
+            break
+    else:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
--- parallel_video.cu
+++ parallel_video.cu
@@ -1,7 +1,8 @@
-// nvcc parallel_linear_interpolation.cu -lX11 -o parallel_BLI
+// nvcc parallel_video.cu -lX11
 
 
 #include <iostream>
+#include <vector>
 #include <dirent.h>
 #include <cuda_runtime.h>
 #include "CImg.h"
@@ -12,7 +13,9 @@ using namespace std;
 
 static int block_size = 1024;
 
-static int pixel_per_thread = 8;
+static int pixel_per_thread = 32;
+
+static int img_per_kernel = 2;
 
 __device__ int pixel(unsigned char *img, int x, int y, int width, int size, int rgb){
     return img[(x) + (y)*width + size * rgb];
@@ -38,21 +41,64 @@ __global__ void linear_interpolation(unsigned char *d_old_image, unsigned char *
     }
 }
 
-__global__ void nearest_neighbor_interpolation(unsigned char *d_old_image, unsigned char *d_new_image, int old_width, int old_height, int new_width, int new_height, int pixel_per_thread){
+__global__ void nearest_neighbor_interpolation(unsigned char *d_old_image, unsigned char *d_new_image, int old_width, int old_height, int new_width, int new_height, int pixel_per_thread, int imgs_size){
     int old_size = old_height * old_width;
     int new_size = new_height * new_width;
     float scale = (float)new_width / old_width;
     for (int i = 0; i < pixel_per_thread; i++){
-        int pos = pixel_per_thread *(blockIdx.x * blockDim.x + threadIdx.x) + i;
-        int pos_x = (pos % new_size) % new_width;
-        int pos_y = (pos % new_size) / new_width;
-        int r_g_b = pos / new_size;
-        if (pos_x >= new_width || pos_y >= new_height) continue;
-        d_new_image[pos_x + pos_y * new_width + new_size * r_g_b] = d_old_image[(int)(pos_x / scale) + (int)(pos_y / scale) * old_width + old_size * r_g_b];
+        int pos = pixel_per_thread * (blockIdx.x * blockDim.x + threadIdx.x) + i;
+        int img = pos /(new_size * 3);
+        int img_pos = pos %(new_size * 3);
+        int pos_x = (img_pos % new_size) % new_width;
+        int pos_y = (img_pos % new_size) / new_width;
+        int r_g_b = img_pos / new_size;
+        if (img > imgs_size) break;
+        d_new_image[img * new_size * 3 + pos_x + pos_y * new_width + new_size * r_g_b] = d_old_image[img * old_size * 3 + (int)(pos_x / scale) + (int)(pos_y / scale) * old_width + old_size * r_g_b];
     }
 }
 
-void interpolate(string path, string file_name, int scale, int interpolation_mode){
+void interpolate(vector<string> paths, vector<string> file_names, int scale, int interpolation_mode){
+    unsigned long long old_size = 0;
+    unsigned long long new_size = 0;
+    unsigned char *d_old_images;
+    unsigned char *d_new_images;
+    vector<CImg<unsigned char>> old_imgs;
+    vector<CImg<unsigned char>> new_imgs;
+    for (int i = 0; i < paths.size(); i++){
+        old_imgs.push_back(CImg<unsigned char>(paths.at(i).c_str()));
+        old_size += old_imgs.at(i).size();
+    }
+
+    for (int i = 0; i < paths.size(); i++){
+        if(interpolation_mode == 1) new_imgs.push_back(CImg<unsigned char>(old_imgs.at(i).width() * scale, old_imgs.at(i).height() * scale, 1, 3, 255));
+        if(interpolation_mode == 2) new_imgs.push_back(CImg<unsigned char>(old_imgs.at(i).width()* scale - (scale - 1), old_imgs.at(i).height() * scale - (scale - 1), 1, 3, 255));
+        new_size += new_imgs.at(i).size();
+    }
+    cudaMalloc((void **)&d_old_images, old_size);
+    cudaMalloc((void **)&d_new_images, new_size);
+
+    for (int i = 0; i < paths.size(); i++){
+        cudaMemcpy(d_old_images + i * old_imgs.at(i).size(), old_imgs.at(i).data(), old_imgs.at(i).size(), cudaMemcpyHostToDevice);
+    }
+    dim3 blkDim (block_size, 1, 1);
+    dim3 grdDim ((((new_size/paths.size()) + block_size - 1)/block_size + pixel_per_thread - 1)/pixel_per_thread, 1, 1);
+
+    if(interpolation_mode == 1) nearest_neighbor_interpolation<<<grdDim, blkDim>>>(d_old_images, d_new_images, old_imgs.at(0).width(), old_imgs.at(0).height(), new_imgs.at(0).width(), new_imgs.at(0).height(), pixel_per_thread, paths.size());
+    if(interpolation_mode == 2) linear_interpolation<<<grdDim, blkDim>>>(d_old_images, d_new_images, old_imgs.at(0).width(), old_imgs.at(0).height(), new_imgs.at(0).width(), new_imgs.at(0).height(), scale, pixel_per_thread);
+
+    cudaDeviceSynchronize();
+
+    for (int i = 0; i < paths.size(); i++){
+        cudaMemcpy(new_imgs.at(i).data(), d_new_images + i * new_imgs.at(0).size(), new_imgs.at(i).size(), cudaMemcpyDeviceToHost);
+    }
+    for (int i = 0; i < paths.size(); i++){
+        cout << "Guardando " << file_names.at(i) << "..." << endl;
+        string _ = "new_imgs/";
+        _.append(file_names.at(i));
+        new_imgs.at(i).save(_.c_str());
+    }
+
+    /*
     CImg<unsigned char> img_in(path.c_str());
     int old_size = img_in.size() / 3;
     int old_width = img_in.width();
@@ -74,7 +120,7 @@ void interpolate(string path, string file_name, int scale, int interpolation_mod
     cudaMalloc((void **)&d_old_image, old_size * 3 * sizeof(unsigned char));
     cudaMalloc((void **)&d_new_image, new_size * 3 * sizeof(unsigned char));
 
-    TIMERSTART(parallel);
+    //TIMERSTART(parallel);
     cudaMemcpy(d_old_image, old_image, old_size * 3, cudaMemcpyHostToDevice);
 
     dim3 blkDim (block_size, 1, 1);
@@ -87,14 +133,17 @@ void interpolate(string path, string file_name, int scale, int interpolation_mod
 
     cudaMemcpy(new_image, d_new_image, new_size * 3, cudaMemcpyDeviceToHost);
 
-    TIMERSTOP(parallel);
+    //TIMERSTOP(parallel);
 
-    string _ = "new/";
+    string _ = "new_imgs/";
     _.append(file_name);
-    cout << "saving " << file_name << "..." << endl;
+    //cout << "saving " << file_name << "..." << endl;
     img_out.save(_.c_str());
     cudaFree(d_old_image);
-    cudaFree(new_image);
+    cudaFree(d_new_image);
+    */
+    cudaFree(d_old_images);
+    cudaFree(d_new_images);
 }
 
 int main(int argc, char const *argv[]){
@@ -124,17 +173,27 @@ int main(int argc, char const *argv[]){
         }
     }
     path = argv[1];
-
-    if (auto dir = opendir(path.c_str())) { // leer todos los archivos de una carpeta
+    TIMERSTART(ALL_IMGS);
+    // leer todos los archivos de una carpeta
+    vector<string> imgs;
+    vector<string> names;
+    if (auto dir = opendir(path.c_str())) {
         while (auto f = readdir(dir)) {
             if (!f->d_name || f->d_name[0] == '.') continue; // Skip everything that starts with a dot
-            printf("Processing %s...\n", f->d_name);
             string _ = argv[1];
             if(_.at(_.length() - 1) != '/') _.append("/");
             _.append(f->d_name);
-            interpolate(_, f->d_name, scale, interpolation_mode);
+            imgs.push_back(_);
+            names.push_back(f->d_name);
+            if(imgs.size() == img_per_kernel){
+                interpolate(imgs, names, scale, interpolation_mode);
+                imgs.clear();
+                names.clear();
+            }
         }
+        interpolate(imgs, names, scale, interpolation_mode);
         closedir(dir);
     }
+    TIMERSTOP(ALL_IMGS);
     return 0;
 }
